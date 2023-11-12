b4fb00e0542f42b5e80a1fb619d9576d9184e0c7
Suyash Chavan <suyashchavanofficial@gmail.com>
2023-11-12 15:33:52 +0530

    Update day-17-easy.cpp

--- day-17-easy.cpp
+++ day-17-easy.cpp
@@ -1,21 +1,10 @@
 int gearSelection(int n) {
-    // 2 2
-    // 3 6
-    // 4 12
     int count=0;
 
     if(n==2) {
         return 2;
-    }
-
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(i != j) {
-                count++;
-            } else {
-                continue;
-            }
-        }
+    } else {
+        count = (n-1)*n;
     }
 
     return count;
